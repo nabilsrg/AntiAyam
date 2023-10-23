ca682758cbce82362805839db568a2e7da5a3632
nabilsrg <nabilsiregar70@gmail.com>
2023-10-23 20:11:45 +0700

    delete some comments in userRoutes.js

--- app/Routes/UserRoutes.js
+++ app/Routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
+router.get('/', UserController.page_login)
 // Rute pendaftaran pengguna
 router.post('/register', UserController.register);
 

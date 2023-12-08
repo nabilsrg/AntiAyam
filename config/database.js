656f254f2eea09f36172d399af223740d9d261fd
nabilsrg <nabilsiregar70@gmail.com>
2023-12-08 19:55:53 +0700

    change connection aws rds

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    host: "antiayam-db.cfvckgueuywx.us-east-1.rds.amazonaws.com",
+    host: "antiayam-db.cs7hxfjx0nf2.ap-southeast-1.rds.amazonaws.com",
     user: "admin",
     password: "antiayam987",
     database: "antiayam"
